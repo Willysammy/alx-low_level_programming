6a85011ffe6d532ff9119213fec38ba02a735e8e
Samuel <olorunfemi693@gmail.com>
2022-09-08 14:41:58 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
- * main - Betty
- * Using printf
+ * Main - Betty
+ * Using printf as function
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }
 
