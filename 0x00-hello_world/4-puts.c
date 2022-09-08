00ee5e61e141a8b3b6bf9e06033e1de09a43a9cb
Samuel <olorunfemi693@gmail.com>
2022-09-08 13:03:35 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /* printing an output using puts*/
-int main(void)
+int main(void)/*printing using the puts function instead of printf*/
 {
 puts("Programming is like building a multilingual puzzle");
 putchar(0);
