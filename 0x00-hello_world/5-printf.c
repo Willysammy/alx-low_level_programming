b9cf62d4534465149150548b3860a280538ab840
Samuel <olorunfemi693@gmail.com>
2022-09-08 15:06:29 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
- * Main - Betty
- * Using printf as function
- * I have to do this
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-int main(void)/*Main has a description now*/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
