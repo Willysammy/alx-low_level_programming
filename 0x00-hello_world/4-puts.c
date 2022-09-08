6a4d4ede5301936b95555c5672e4971cdbf3d472
Samuel <olorunfemi693@gmail.com>
2022-09-08 12:25:57 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 /* printing an output using puts*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle %d.\n", 0)
-		return(0)
+	puts("Programming is like building a multilingual puzzle");
+	puts("0");
+
+	getchar();
+	return 0;
 }
+
