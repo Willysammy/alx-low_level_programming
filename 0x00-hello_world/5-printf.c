6d9f2e3d95777522619b7317978eca56c6b6ece1
Samuel <olorunfemi693@gmail.com>
2022-09-08 14:47:54 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 /*
  * Main - Betty
  * Using printf as function
+ * I have to do this
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
