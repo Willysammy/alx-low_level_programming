c0e8c6da802181bda6c7409f494a9c13efd7549c
Samuel <olorunfemi693@gmail.com>
2022-09-08 18:26:45 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,21 @@
-@@ -3,8 +3,7 @@
+#include <stdio.h>
 /**
- * main - to produce the size of strings
+ * main - print the sizeof variable types
  *
- * Description: produce size of strings in byte
- * Return: 0
+ * Return: Always return 0 (Success)
  */
 
-@@ -18,8 +17,8 @@ int main(void)
+int main(void)
 {
-printf(”Size of a char: %ld byte(s)\n”, sizeof(c));
-printf(”Size of an int: %ld byte(s)\n”, sizeof(i));
-    printf(”Size of long int: %ld byte(s)\n”, sizeof(li));
-    printf(”Size of a long long int: %ld byte(s)\n”, sizeof(lli));
-printf(”Size of a float: %ld byte(s)\n”, sizeof(f));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+printf("Size of a char: %d byte(s)\n", sizeof(a));
+printf("Size of an int: %d byte(s)\n", sizeof(b));printf("Size of a long int: %d byte(s)\n", sizeof(c));
+printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+printf("Size of a float: %d byte(s)\n", sizeof(e));
 
-return (0);
+return (0)
 }
