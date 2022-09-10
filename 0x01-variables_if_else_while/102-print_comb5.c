f9e56b95ccf163d06ce3d55c7251a0b886a86815
Samuel <olorunfemi693@gmail.com>
2022-09-10 08:48:04 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int digit1, digit2, digit3, digit4;
 
-	for (digit1 = 0; digit1 <10; digit1++)
+	for (digit1 = 0; digit1 < 10; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
