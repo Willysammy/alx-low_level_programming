f62fe141062f087b4e5624c2b2e85783354ec39c
Samuel <olorunfemi693@gmail.com>
2022-09-10 04:29:06 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print numbers in base ten
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i, i\n", i);
+	}
+
+	return (0);
+
+}
