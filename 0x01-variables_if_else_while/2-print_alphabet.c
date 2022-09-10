3e81e4294bc202e5093beea6cc1e69b3b55a4c87
Samuel <olorunfemi693@gmail.com>
2022-09-09 20:26:50 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,14 @@
  */
 int main(void)
 {
-char x;
-for (x = "a"; x <= "z"; x++)
-{
-putchar (x);
-}
-putchar ("\n");
+	char x;
+	for (x = "a"; x <= "z"; x++)
+	{	
+		putchar(x);
+	}
+
+putchar("\n");
 
 return (0);
+
 }
