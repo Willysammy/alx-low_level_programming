fae3616425192077674d16712783db498ad5c4dc
Samuel <olorunfemi693@gmail.com>
2022-09-13 16:14:37 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,16 @@
  */
 void print_alphabet_x10(void)
 {
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(c ** 10);
+		for (c= 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
 	}
-	_putchar('\n');
+	return (0);
 }
