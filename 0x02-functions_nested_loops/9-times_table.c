e073ff621a755292c589e541f65a2acd23a65df2
Samuel <olorunfemi693@gmail.com>
2022-09-13 18:10:26 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * times_table - Function that print the multiplication table
+ * @i, r: Variables to declare
+ * Return: void
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= ; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(i * j);
+			_putchar(',');
+			_putchar('\n');
+		}
+	}
+}
