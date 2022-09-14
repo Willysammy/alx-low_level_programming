6456ce28c20e0e8884f1c6d59faecf17be629e9b
Samuel <olorunfemi693@gmail.com>
2022-09-13 18:39:58 -0700

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** add - Function to add two ints and return the result
+/**
+ * add - Function to add two ints and return the result
  *
  * @a, b: variables to declare
  * Return: Return value
@@ -8,6 +9,7 @@
 int add(int a, int b)
 {
 	int result = a + b;
+
 	return (result);
 
 	add(2, 5);
