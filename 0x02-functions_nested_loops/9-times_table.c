23c6c25613bebda16fa6050ea66c97429db6fb78
Samuel <olorunfemi693@gmail.com>
2022-09-13 18:23:45 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,14 +9,29 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int k;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+
+		for (j = 1; j <= 9; j++)
 		{
-			_putchar(i * j);
 			_putchar(',');
-			_putchar('\n');
+			_putchar(' ');
+
+			k = i * j;
+			if (k <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((k / 10) + '0');
+			}
+			_putchar((k % 10) + '0');
 		}
+
+		_putchar('\n');
 	}
 }
