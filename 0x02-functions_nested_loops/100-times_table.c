5aaaba3c6c4df83634335442c5c96a3bf3c570d9
Samuel <olorunfemi693@gmail.com>
2022-09-14 02:42:12 -0700

    Task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_times_table - Function to print the mutiplication tables of digits less than 15
+ *
+ * @n: Variable to declare
+ * Return: Always void
+ */
+void print_times_tables(int n)
+{
+	int p;
+	int prod;
+
+	for (p = 0; p <= 12; p++)
+	{
+		printf("0\n");
+		for (n = 0; n <= 15; n++)
+		{
+			printf("%d\n", n);
+			prod = p * n;
+			printf("%d\n", prod);
+
+			if (n > 15 || n < 0)
+			{
+				continue;
+			}
+		}
+	}
+}
