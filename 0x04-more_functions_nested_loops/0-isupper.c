21d3709b3b4488c4517607580a1995c99bb67f9a
Samuel <olorunfemi693@gmail.com>
2022-09-15 15:39:27 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/** _isupper - Functio to print is integer is upper or not
+ *
+ * Return: returns 1 otherwise 0
+ */
+int _isupper(int c)
+{
+	for (c = 65; c <= 90; c++)
+	{
+		if (c = 97 || c <= 122)
+		{
+			return (0);
+			continue;
+		}
+	}
+	_putchar('\n');
+	return (1);
+}
