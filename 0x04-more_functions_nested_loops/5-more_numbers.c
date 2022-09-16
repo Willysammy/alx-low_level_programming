83a70484f8ce32c0387ebaec156bd9e1df456c7b
Samuel <olorunfemi693@gmail.com>
2022-09-15 17:40:52 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more numbers
+ */
+void  more_numbers(void)
+{
+	int c, i;
+
+	for (c = 0; c <= 10; c++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
