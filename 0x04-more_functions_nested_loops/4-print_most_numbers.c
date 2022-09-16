9eb63ac061fe1da292702807740348f0079691c1
Samuel <olorunfemi693@gmail.com>
2022-09-15 17:23:51 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Function to print most numbers
+ * @c: variable
+ *
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		if (c == 50 || c == 52)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
