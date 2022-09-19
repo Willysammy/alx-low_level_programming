57121188962f3b325f6a9ba71a0e6f471350574b
Samuel <olorunfemi693@gmail.com>
2022-09-19 10:45:39 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - To print length of char
+ * @s: Variable
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = _strlen(s);
+	return (len);
+}
