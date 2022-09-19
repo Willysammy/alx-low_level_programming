ad5a56158341952c35dd624414658bed456a7ae8
Samuel <olorunfemi693@gmail.com>
2022-09-19 16:32:16 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: variable
+ *
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	int count = 0;
+	int end;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	for (end = count - 1; end >= 0; end--)
+	{
+		_putchar(s[end]);
+	}
+}
