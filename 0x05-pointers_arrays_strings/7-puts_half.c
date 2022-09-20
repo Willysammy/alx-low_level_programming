b45bdeb0a6a6e94f609c295a95e89df2cdc455f1
Samuel <olorunfemi693@gmail.com>
2022-09-19 17:31:49 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,4 +8,29 @@
  */
 void puts_half(char *str)
 {
+	int count, half;
 
+	count = 0;
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			break;
+			count++;
+		}
+		if (count % 2 == 1)
+		{
+			half = count % 2;
+		}
+		else
+		{
+			half = (count -1) / 2;
+		}
+
+		for (half++; half < count; half++)
+		{
+			_putchar(str[half]);
+		}
+		_putchar('\n');
+	}
+}
