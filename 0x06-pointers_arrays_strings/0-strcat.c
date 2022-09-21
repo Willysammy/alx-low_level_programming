6efec4fbe0d748a076c542d5fc6c2e6b1889837e
Samuel <olorunfemi693@gmail.com>
2022-09-21 07:20:27 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strcat - To concatenate two strings
+ * @dest: first variable
+ * @src: second variable
+ * Return: Always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	dest[];
+	src[];
+	int c,d;
+
+	c = 0;
+	while (dest[c] != '\0')
+		c++;
+	d = 0;
+
+	while (src[d] != '\0')
+	{
+		d++;
+		c+=;
+	}
+	dest++;
+	return (dest);
+}
