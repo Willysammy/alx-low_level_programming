510e395930fc273e9112603444c82cfc3d5c07bc
Samuel <olorunfemi693@gmail.com>
2022-09-27 12:09:04 -0700

    Task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - set pointer to a char
+ * @s: double pointer
+ * @to: normal pointer
+ * Return: Void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
