f901bb6119a3c71ce40ffb589ed059e4857d6802
Samuel <olorunfemi693@gmail.com>
2022-09-30 09:28:29 -0700

    Task 4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,7 +5,7 @@
  * main - function main
  * @argc: argument count
  * @argv: argument vector
- * Return: Return 0 on success else 1
+ * Return:0 on success else 1
  */
 int main(int argc, char *argv[])
 {
@@ -16,14 +16,9 @@ int main(int argc, char *argv[])
 	{
 		s = atoi(argv[1]);
 		j = atoi(argv[2]);
-
-		printf("%i\n", s *j);
-
+		printf("%i\n", s * j);
 		return (0);
 	}
-	else
-	{
-		printf("error\n");
-		return (1);
-	}
+	printf("error\n");
+	return (1);
 }
