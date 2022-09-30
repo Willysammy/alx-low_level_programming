be8ff66108c621df193c35bf0c8d37a018b532d0
Samuel <olorunfemi693@gmail.com>
2022-09-30 06:57:47 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0n success 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%i\n", argc);
+
+	return (0);
+}
