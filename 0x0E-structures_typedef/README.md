e01e9be7bcfcc7c192e704f95b518e366bd58105
Samuel <olorunfemi693@gmail.com>
2022-10-10 07:43:42 -0700

    Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This repository contains alx tasks on C structures and typedef.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * struct dog - my structure name
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ * Return: 0
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
