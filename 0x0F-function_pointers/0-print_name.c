1f0f212fbc7756be1975f0a818afae21391835e1
Samuel <olorunfemi693@gmail.com>
2022-10-12 04:34:41 -0700

    Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: pointer to char
+ * @f: pointer to the function
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This repository contains tasks on function pointers and call-backs
