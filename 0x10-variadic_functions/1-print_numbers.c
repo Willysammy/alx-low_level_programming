fecf12afd2861d7a58e9c0a5316f297e2b3163aa
Samuel <olorunfemi693@gmail.com>
2022-10-13 11:59:46 -0700

    Print int

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints integers separated by strings
+ * @separator: separating string
+ * @n: number of integers
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int count;
+
+
+	if (separator == NULL)
+		return;
+	va_start(ap, n);
+	count = 0;
+	while (count < n)
+	{
+		printf("%d", va_arg(ap, int));
+		if (!'\0')
+		{
+			printf("%s", separator);
+		}
+		count++;
+	}
+	va_end(ap);
+	printf("\n");
+}
